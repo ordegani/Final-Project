9436dde82c37f472b7fa6c58fd3cb0f02cc70043
ordegani <oradeganii@gmail.com>
2021-08-18 14:14:09 +0300

    changes

--- client/src/components/exercises-list.js
+++ client/src/components/exercises-list.js
@@ -6,7 +6,7 @@ import "../components/form/maincontainer.css";
 const Exercise = props => (
   <tr>
     <td>{props.exercise.description}</td>
-    <td>{props.exercise.descriptio}</td>
+    <td>{props.exercise.description2}</td>
     <td>{props.exercise.date.substring(0,10)}</td>
     <td>
        <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
