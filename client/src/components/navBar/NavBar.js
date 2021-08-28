65110184ce51caa2335307d6149e0f22795fabb0
ordegani <oradeganii@gmail.com>
2021-08-28 17:03:10 +0300

    changes

--- client/src/components/navBar/NavBar.js
+++ client/src/components/navBar/NavBar.js
@@ -7,29 +7,29 @@ export default class Navbar extends Component {
   render() {
     return (
 
-      <nav className="navbar">
+      // <nav className="navbar">
         
-        <div className="collpase navbar-collapse">         
-        <ul className="navbar-items">
-          {/* TODO about page */}
-          <Link to="/about" className="navbar-brand">ArtNotes</Link>
-        <li className="navbar-item">
-          <Link to="/search" className="nav-link">Search</Link>
-          </li>
-        <li className="navbar-item">
-          <Link to="/" className="nav-link">Discover</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Note</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/list" className="nav-link">Notes List</Link>
-          </li>
+      //   <div className="collpase navbar-collapse">         
+      //   <ul className="navbar-items">
+      //     {/* TODO about page */}
+      //     <Link to="/about" className="navbar-brand">ArtNotes</Link>
+      //   <li className="navbar-item">
+      //     <Link to="/search" className="nav-link">Search</Link>
+      //     </li>
+      //   <li className="navbar-item">
+      //     <Link to="/" className="nav-link">Discover</Link>
+      //     </li>
+      //     <li className="navbar-item">
+      //     <Link to="/create" className="nav-link">Create Note</Link>
+      //     </li>
+      //     <li className="navbar-item">
+      //     <Link to="/list" className="nav-link">Notes List</Link>
+      //     </li>
 
-        </ul>
+      //   </ul>
         
-        </div>
-      </nav>
+      //   </div>
+      // </nav>
     );
   }
 }
