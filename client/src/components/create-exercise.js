d488e68d9d1f7afe12e8596329afbf2710c571cc
ordegani <oradeganii@gmail.com>
2021-09-22 18:47:27 +0300

    changes

--- client/src/components/create-exercise.js
+++ client/src/components/create-exercise.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import DatePicker from 'react-datepicker';
+import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 // import Form from "./form/Form"
 import "../components/form/maincontainer.css";
-import "./form/maincontainer.css"
+import "./form/maincontainer.css";
 import { withRouter } from "react-router";
 import Form from "./form/Form";
 
 class CreateExercise extends Component {
-
   constructor(props) {
     super(props);
     this.onChangeDescription = this.onChangeDescription.bind(this);
@@ -18,43 +17,43 @@ class CreateExercise extends Component {
     this.onChangeDate = this.onChangeDate.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
-
-
     this.state = {
-
-      description: this.props.save.title +', ' +this.props.save.artistName+', '+this.props.save.completitionYear ,
-      descriptio: 
-
-      "for more info: "+`https://www.wikiart.org/en/${this.props.save.title}/`,
+      description:
+        this.props.save.title +
+        ", " +
+        this.props.save.artistName +
+        ", " +
+        this.props.save.completitionYear,
+      descriptio:
+        "for more info: " +
+        `https://www.wikiart.org/en/${this.props.save.title}/`,
       duration: 0,
       date: new Date(),
-  
-    }
+    };
   }
-
 
   onChangeDescription(e) {
     this.setState({
-      description: e.target.value
-    })
+      description: e.target.value,
+    });
   }
 
   onChangeDescriptio(e) {
     this.setState({
-      descriptio: e.target.value
-    })
+      descriptio: e.target.value,
+    });
   }
 
   onChangeDuration(e) {
     this.setState({
-      duration: e.target.value
-    })
+      duration: e.target.value,
+    });
   }
 
   onChangeDate(date) {
     this.setState({
-      date: date
-    })
+      date: date,
+    });
   }
 
   onSubmit(e) {
@@ -66,43 +65,52 @@ class CreateExercise extends Component {
       duration: this.state.duration,
       date: this.state.date,
       // id:this.state.id
-   
-    }
+    };
 
     console.log(exercise);
 
-    axios.post('https://artnote.herokuapp.com/exercise', exercise)
-      .then(res => this.props.history.push('/list'));
+    axios
+      .post("https://artnote.herokuapp.com/exercise", exercise)
+      .then((res) => this.props.history.push("/list"));
     // window.location = '/';
   }
 
   render() {
     return (
-    <div className="noteImg">
-            
-      {this.props.save && <img className="image" src={this.props.save.image} alt="image"></img>}
-      <form className="form" onSubmit={this.onSubmit}>
-   
-        <div className="form-group"> 
-          <label>Title and Description: </label>
-          <input  type="text"
-              required
-              className="form-control" 
-              value={(this.props.save)? this.state.description: "Oops, you didn't choose an artwork"} 
-              onChange={this.onChangeDescription}
-        
-              />
-        </div>
-
-        <label>Personal Notes: </label>
-          <input  type="text"
+      <div className="noteImg">
+        {this.props.save && (
+          <img className="image" src={this.props.save.image} alt="image"></img>
+        )}
+        <form className="form" onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Title and Description: </label>
+            <input
+              type="text"
               required
               className="form-control"
-              value={(this.props.save)? (this.props.save && this.state.descriptio):"Choose an artwork and fill me up"}
-              onChange={this.onChangeDescriptio}
-              />
-       
-{/* 
+              value={
+                this.props.save
+                  ? this.state.description
+                  : "Oops, you didn't choose an artwork"
+              }
+              onChange={this.onChangeDescription}
+            />
+          </div>
+
+          <label>Personal Notes: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={
+              this.props.save
+                ? this.props.save && this.state.descriptio
+                : "Choose an artwork and fill me up"
+            }
+            onChange={this.onChangeDescriptio}
+          />
+
+          {/* 
         <div className="form-group"> 
     <label>Duration (in minutes): </label>
           <input 
@@ -112,28 +120,24 @@ class CreateExercise extends Component {
               onChange={this.onChangeDuration}
               />
         </div>  */}
-        <div className="form-group">
-          <label>Date: </label>
-          <div>
-            <DatePicker
-              selected={this.state.date}
-              onChange={this.onChangeDate}
-            />
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+            <div></div>
           </div>
-          <div>
 
-       </div>
-        </div>
-
-        
-        <div className="form-group">
-          <input type="submit" value="Save" className="createNoteBtn" />
-        </div>
-      </form>
-    </div>
-    )
+          <div className="form-group">
+            <input type="submit" value="Save" className="createNoteBtn" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
-
 
 export default withRouter(CreateExercise);
