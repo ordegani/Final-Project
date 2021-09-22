de3424e1270cabf5ec59f4ade4a68b6a423b42a2
ordegani <oradeganii@gmail.com>
2021-09-22 18:48:43 +0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import About from "./components/about"
-import Navbar from "./components/navBar/NavBar"
+import About from "./components/about";
+import Navbar from "./components/navBar/NavBar";
 import ExercisesList from "./components/exercises-list";
 import CreateExercise from "./components/create-exercise";
 import Form from "./components/form/Form";
-import Search from "./components/Search"
-
-
+import Search from "./components/Search";
 
 function App() {
   const [save, setsave] = useState(false);
- return (
-  <BrowserRouter>
+  return (
+    <BrowserRouter>
       <div className="container">
-      <Navbar />
-      <Switch>
+        <Navbar />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/search" exact>
+            <Search setsave={setsave} save={save} />
+          </Route>
+          {/* <Route path="/discover" component={Discover} /> */}
+          <Route path="/" exact>
+            <Form setsave={setsave} save={save} />
+          </Route>
 
-      <Route path="/about" component={About} />
-      <Route path="/search" exact> 
-      <Search setsave={setsave} save={save}/>
-      </Route>
-      {/* <Route path="/discover" component={Discover} /> */}
-      <Route path="/" exact> 
-      <Form setsave={setsave} save={save}/>
-      </Route>
-
-
-      <Route path="/create" exact> 
-      <CreateExercise save={save}/>
-      </Route>
-      <Route path="/list" exact component={ExercisesList} />
-      {/* <Route path="/create" component={CreateExercise} /> */}
-      </Switch>
+          <Route path="/create" exact>
+            <CreateExercise save={save} />
+          </Route>
+          <Route path="/list" exact component={ExercisesList} />
+          {/* <Route path="/create" component={CreateExercise} /> */}
+        </Switch>
       </div>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
