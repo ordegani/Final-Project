830ff5b4b13048a613e8f0f1cc2b69bc4478846f
ordegani <oradeganii@gmail.com>
2021-09-22 18:51:12 +0300

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
-const express = require('express');
-const path = require ('path');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,19 +11,22 @@ app.use(cors());
 app.use(express.json());
 console.log(process.env.ATLAS_URI);
 
-
-const uri = "mongodb+srv://or:or@cluster0.yhueg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true  }
-);
+const uri =
+  "mongodb+srv://or:or@cluster0.yhueg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
 
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
-const exercisesRouter = require('./routes/exercise');
+const exercisesRouter = require("./routes/exercise");
 
-app.use('/exercise', exercisesRouter);
+app.use("/exercise", exercisesRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
@@ -34,5 +35,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
