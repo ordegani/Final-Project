e999864977edd9d67aadf6ad0ebb2dfb66030227
ordegani <oradeganii@gmail.com>
2021-09-22 18:47:43 +0300

    changes

--- client/src/components/exercises-list.js
+++ client/src/components/exercises-list.js
@@ -1,53 +1,68 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import "../components/form/maincontainer.css";
 
-const Exercise = props => (
+const Exercise = (props) => (
   <tr>
     <td>{props.exercise.description}</td>
     <td>{props.exercise.descriptio}</td>
-    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
     <td>
-       <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExercise(props.exercise._id);
+        }}
+      >
+        delete
+      </a>
     </td>
-    
   </tr>
-)
+);
 
 export default class ExercisesList extends Component {
   constructor(props) {
     super(props);
 
-    this.deleteExercise = this.deleteExercise.bind(this)
+    this.deleteExercise = this.deleteExercise.bind(this);
 
-    this.state = {exercises: []};
+    this.state = { exercises: [] };
   }
 
   componentDidMount() {
-    axios.get('https://artnote.herokuapp.com/exercise')
-      .then(response => {
-        this.setState({ exercises: response.data })
+    axios
+      .get("https://artnote.herokuapp.com/exercise")
+      .then((response) => {
+        this.setState({ exercises: response.data });
       })
       .catch((error) => {
         console.log(error);
-      })
+      });
   }
 
   deleteExercise(id) {
-    console.log(id)
-    axios.delete(`https://artnote.herokuapp.com/exercise/`+id)
-      .then(response => { console.log(response.data)});
+    console.log(id);
+    axios
+      .delete(`https://artnote.herokuapp.com/exercise/` + id)
+      .then((response) => {
+        console.log(response.data);
+      });
 
     this.setState({
-      exercises: this.state.exercises.filter(el => el._id !== id)
-    })
-  
+      exercises: this.state.exercises.filter((el) => el._id !== id),
+    });
   }
   exerciseList() {
-    return this.state.exercises.map(currentexercise => {
-      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
-    })
+    return this.state.exercises.map((currentexercise) => {
+      return (
+        <Exercise
+          exercise={currentexercise}
+          deleteExercise={this.deleteExercise}
+          key={currentexercise._id}
+        />
+      );
+    });
   }
 
   render() {
@@ -62,13 +77,9 @@ export default class ExercisesList extends Component {
               <th>Date</th>
             </tr>
           </thead>
-          <tbody>
-            { this.exerciseList() }
-          </tbody>
+          <tbody>{this.exerciseList()}</tbody>
         </table>
       </div>
-    )
+    );
   }
 }
-
-
